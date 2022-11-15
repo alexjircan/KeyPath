beb5750915474d7648071fae236fdf63f5e0c6c6
Alexandru Jircan <alex_jircan@yahoo.com>
2022-11-15 22:02:32 +0200

    login/register update

--- ui/src/app/modules/register/register.module.ts
+++ ui/src/app/modules/register/register.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RegisterComponent } from "./register.components";
@@ -5,6 +6,6 @@ import { RegisterComponent } from "./register.components";
 
 @NgModule({
     declarations: [RegisterComponent],
-    imports: [FormsModule, ReactiveFormsModule],
+    imports: [FormsModule, ReactiveFormsModule, CommonModule],
 })
 export class RegisterModule { } 
