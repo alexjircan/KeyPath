658f977223229c032a3f418248025c0a3c9b79ef
Alexandru Jircan <alex_jircan@yahoo.com>
2022-11-28 19:36:38 +0200

    frontend login update + pipfile

--- ui/src/app/core/auth/auth.module.ts
+++ ui/src/app/core/auth/auth.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from "@angular/core";
+import { JwtModule } from "@auth0/angular-jwt";
 import { AuthService } from "./auth.service";
+import { environment } from "@/environments/environment";
+
+export function tokenGetter() {
+    return localStorage.getItem(environment.jwt.tokenKey);
+  }
 
 @NgModule({
     providers: [AuthService],
+    imports: [
+        JwtModule.forRoot({
+            config: {
+                authScheme: 'bearer ',
+                tokenGetter,
+                skipWhenExpired: false,
+                allowedDomains: environment.jwt.allowedDomains,
+                disallowedRoutes: environment.jwt.disallowedRoutes,
+              },
+        })
+    ],
     exports: []
 })
 export class AuthModule {
--- ui/src/app/modules/login/login.component.ts
+++ ui/src/app/modules/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from "@/app/core/auth/auth.service";
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
 
@@ -11,14 +12,20 @@ export class LoginComponent implements OnInit{
     @ViewChild('password') private passwordInput;
     @ViewChild('username') private usernameInput;
 
+    isLoading: boolean = false;
+
     public form: FormGroup<{
-        email: FormControl<string>;
+        username: FormControl<string>;
         password: FormControl<string>;
       }>;
 
+    constructor(
+    public $auth: AuthService,
+    ) { }
+
     ngOnInit(): void {
         this.form = new FormGroup({
-            email: new FormControl('', [Validators.email]),
+            username: new FormControl('', [Validators.email]),
             password: new FormControl(''),
           });
     }
@@ -27,13 +34,21 @@ export class LoginComponent implements OnInit{
         // grab the values from the native element as they may be "populated" via autofill.
         const passwordInputValue = this.passwordInput?.nativeElement.value;
         if (passwordInputValue && passwordInputValue !== this.form.get('password').value) {
-        this.form.controls.password.setValue(passwordInputValue);
+            this.form.controls.password.setValue(passwordInputValue);
         }
 
         const usernameInputValue = this.usernameInput?.nativeElement.value;
         if (usernameInputValue && usernameInputValue !== this.form.get('username').value) {
-        this.form.controls.email.setValue(usernameInputValue);
+            this.form.controls.username.setValue(usernameInputValue);
         }
+
+        this.$auth.login(this.form.getRawValue()).subscribe({
+            next(response) { 
+                console.log(response); },
+            error(err) { console.log(err); },
+        }).add( () => this.isLoading = false );
+
+        this.isLoading = true;
     }
 
 }
