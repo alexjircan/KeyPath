2ff159c0d49659b524d629ee1f77d6979a7539ff
Alexandru Jircan <alex_jircan@yahoo.com>
2023-01-03 20:08:14 +0200

    change password

--- ui/src/app/modules/details/details.component.ts
+++ ui/src/app/modules/details/details.component.ts
@@ -1,6 +1,8 @@
+import { AuthService } from "@/app/core/auth/auth.service";
 import { User } from "@/app/core/user";
 import { UserService } from "@/app/core/user.service";
 import { Component, OnInit } from "@angular/core";
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
 import { Observable } from "rxjs";
 
 @Component({
@@ -10,14 +12,44 @@ import { Observable } from "rxjs";
 })
 export class DetailsComponent implements OnInit{
     public user$: Observable<User>;
+    public changePass: boolean = false;
+    public loading:boolean = false;
+    public passChanged: boolean = false;
+
+    public form: FormGroup<{
+        password: FormControl<string>;
+        conPassword: FormControl<string>;
+    }>;
 
     constructor(
-        public $userService: UserService
+        public $userService: UserService,
+        private $auth: AuthService,
     ){
+    }
+
+    checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
+        let pass = group.get('password').value;
+        let confirmPass = group.get('conPassword').value
+        return pass === confirmPass ? null : { notSame: true }
     }
 
     ngOnInit(): void {
         this.user$ = this.$userService.getUserDetails();
+        this.form = new FormGroup({
+            password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d\\w\\W]{8,}$')]),
+            conPassword: new FormControl('', [Validators.required]),
+          }, {validators: [this.checkPasswords]});
+    }
+
+    onSubmit() {
+        this.$auth.changePassword(this.form.get('password').value).subscribe(
+            resp => {
+                this.loading = false;
+                this.changePass = false;
+                this.passChanged = true;
+            }
+        )
+        this.loading = true;
     }
     
 }
--- ui/src/app/modules/details/details.module.ts
+++ ui/src/app/modules/details/details.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { DetailsComponent } from "./details.component";
 
 @NgModule({
     declarations: [DetailsComponent],
-    imports: [CommonModule]
+    imports: [CommonModule, FormsModule, ReactiveFormsModule]
 })
 export class DetailsModule{}
